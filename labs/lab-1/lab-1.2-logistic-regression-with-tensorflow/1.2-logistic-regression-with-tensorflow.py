25e85b6464dd823f7d76ee226a6ad259d13284e3
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-03-25 12:51:36 +0000

    Some minor changes added

--- labs/lab-1/lab-1.2-logistic-regression-with-tensorflow/1.2-logistic-regression-with-tensorflow.py
+++ labs/lab-1/lab-1.2-logistic-regression-with-tensorflow/1.2-logistic-regression-with-tensorflow.py
@@ -65,27 +65,27 @@ def neural_network_prediction(xs_data_features_to_predict):
     return tensorflow.reshape(tensorflow.nn.sigmoid(tensorflow_network, name="output"), [-1])
 
 
-# The function to compute the Cost of the Logistic Loss,
+# The function to compute the Cost of the Logistic Error Loss,
 # between the predicted ys (Labels) of the Data of the Genes,
 # through the Artificial Neural Network (ANN) and the real ys (Labels) of the Data of the Genes
-def compute_logistic_loss(ys_predictions_for_xs_data_features, ys_real_data_labels):
+def compute_logistic_error_loss(ys_predictions_for_xs_data_features, ys_real_data_labels):
 
     # Create a TensorFlow Constant from the given set of ys (Labels) of the Data of the Genes
     tensorflow_constant_ys_predictions_for_xs_data_features = \
         tensorflow.constant(ys_real_data_labels.astype(numpy.float32))
 
-    # Compute the Cost of Logistic Loss
-    logistic_loss_cost = -tensorflow.reduce_mean(
+    # Compute the Cost of Logistic Error Loss
+    logistic_error_loss_cost = -tensorflow.reduce_mean(
         (tensorflow_constant_ys_predictions_for_xs_data_features *
          tensorflow.math.log(ys_predictions_for_xs_data_features)) +
         (1 - tensorflow_constant_ys_predictions_for_xs_data_features) *
         (tensorflow.math.log(1 - ys_predictions_for_xs_data_features)))
 
-    # Return the Cost of the Logistic Loss
-    return logistic_loss_cost
+    # Return the Cost of the Logistic Error Loss
+    return logistic_error_loss_cost
 
 
-# The function to compute the Gradient for the Logistic Loss function
+# The function to compute the Gradient for the Logistic Error Loss function
 def compute_gradient(xs_data_features_to_predict, ys_real_data_labels):
 
     # Create the Gradient Tape to trace all the Computations and
@@ -96,8 +96,8 @@ def compute_gradient(xs_data_features_to_predict, ys_real_data_labels):
         # the Genes given as arguments of the Artificial Neural Network (ANN)
         neural_network_predicted_ys = neural_network_prediction(xs_data_features_to_predict)
 
-        # Compute the Cost of the Logistic Loss function
-        loss_cost_value = compute_logistic_loss(neural_network_predicted_ys, ys_real_data_labels)
+        # Compute the Cost of the Logistic Error Loss function
+        loss_cost_value = compute_logistic_error_loss(neural_network_predicted_ys, ys_real_data_labels)
 
     # Return the Gradient Tape with all the traced Computations and
     # computed Derivatives, as also, the Weights of Neurons and Bias
@@ -145,7 +145,7 @@ def execute_artificial_neural_network():
             batch_ys_data_labels = ys_data_labels[ys_data_labels_shuffled[start_num_sample:
                                                                           (start_num_sample + batch_size)]]
 
-            # Compute the Gradient for the Logistic Loss function for
+            # Compute the Gradient for the Logistic Error Loss function for
             # the chosen Samples from the xs (Features) and ys (Labels) of the Data of the Genes
             gradients, variables = compute_gradient(batch_xs_data_features_standardized, batch_ys_data_labels)
 
@@ -156,12 +156,12 @@ def execute_artificial_neural_network():
         # xs (Features) of the Data of the Genes, using the configured Artificial Neural Network (ANN)
         ys_labels_predicted_for_data = neural_network_prediction(xs_data_features_standardized)
 
-        # Compute the Cost of the Logistic Loss, between the predicted ys (Labels) of the Data of the Genes,
+        # Compute the Cost of the Logistic Error Loss, between the predicted ys (Labels) of the Data of the Genes,
         # through the Artificial Neural Network (ANN) and the real ys (Labels) of the Data of the Genes
-        logistic_loss = compute_logistic_loss(ys_labels_predicted_for_data, ys_data_labels)
+        logistic_loss = compute_logistic_error_loss(ys_labels_predicted_for_data, ys_data_labels)
 
-        # Print the Logistic Loss for the current Epoch of the execution of the Artificial Neural Network (ANN)
-        print(f"Current Epoch: {current_epoch}, Logistic Loss: {logistic_loss}...")
+        # Print the Logistic Error Loss for the current Epoch of the execution of the Artificial Neural Network (ANN)
+        print(f"Current Epoch: {current_epoch}, Logistic Error Loss: {logistic_loss}...")
 
 
 # Print the configuration for the Artificial Neural Network (ANN) being used
