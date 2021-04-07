86aceb6f0ac525e82146c9cea26ff7212fb1fd1b
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-04-07 13:48:38 +0100

    Empty Python File for the Exercise #2 of the Lab #3 added

---
+++ labs/lab-3/exercises/lab-3.2-exploring-hyper-parameters-with-keras-api-for-miles-per-gallon-mpg/3.2-exploring-hyper-parameters-with-keras-api-for-miles-per-gallon-mpg.py
@@ -0,0 +1,8 @@
+"""
+Lab 3.2 - Exploring Hyper-parameters with Keras API for Miles Per Gallon (MPG)
+
+Author:
+- Rodrigo Jorge Ribeiro (rj.ribeiro@campus.fct.unl.pt)
+- Ruben Andre Barreiro (r.barreiro@campus.fct.unl.pt)
+
+"""
